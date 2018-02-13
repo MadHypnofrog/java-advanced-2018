f058e0d57c88aac6fd93029552ae5ce96b30bf17
MadHypnofrog <madhypnofrog@gmail.com>
2018-02-13 22:18:28 +0300

    hw1

--- walk/src/ru/ifmo/rain/kurilenko/walk/Walk.java
+++ walk/src/ru/ifmo/rain/kurilenko/walk/Walk.java
@@ -6,7 +6,7 @@ import java.nio.file.NoSuchFileException;
 public class Walk {
     public static void main(String[] args) {
         try (BufferedReader in = new BufferedReader(new FileReader (new File(args[0]))); BufferedWriter w = new BufferedWriter (new FileWriter(new File(args[1])))) {
-            String path;
+            String path, hash;
             while ((path = in.readLine()) != null) {
                 try (FileInputStream r = new FileInputStream(new File(path))) {
                     int x = 0x811c9dc5, p = 0x01000193, i;
@@ -14,20 +14,25 @@ public class Walk {
                     while ((i = r.read(buf)) >= 0) {
                         for (int tmp = 0; tmp < i; tmp++) x = (x * p) ^ (buf[tmp] & 0xff);
                     }
-                    w.write (String.format("%08x", x) + " " + path + System.lineSeparator());
+                    hash = String.format("%08x", x);
                 } catch (FileNotFoundException e) {
-                    w.write ("00000000 " + path + System.lineSeparator());
+                    hash = "00000000";
                     System.err.println ("Error: file " + path + " not found");
                 }
                 catch (IOException e) {
-                    w.write ("00000000 " + path + System.lineSeparator());
+                    hash = "00000000";
                     System.err.println ("Error: can't read from " + path);
+                }
+                try {
+                    w.write (hash + " " + path + System.lineSeparator());
+                } catch (IOException e) {
+                    System.err.println ("Error: output error");
                 }
             }
         } catch (FileNotFoundException e) {
-            System.err.println ("Error: file " + args[0] + " not found");
+            System.err.println ("Error: input file " + args[0] + " not found");
         } catch (IOException e) {
-            System.err.println ("Error: can't read from " + args[0]);
+            System.err.println ("Error: can't read from input file " + args[0]);
         }
     }
 }
